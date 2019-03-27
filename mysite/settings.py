1f022b0f6e3d3768f81df6ef70d457e5843c1047
anewcookie <33850176+anewcookie@users.noreply.github.com>
2019-03-27 18:46:41 -0400

    collected static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,8 +80,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'defaultAlt': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cadetami-fc40d:us-east4:cadet-ami',
+        'USER': 'ami-app',
+        'PASSWORD': 'majcrosser',
+        'HOST': '35.199.17.43',
     }
 }
+
 
 
 # Password validation
