2f868833a523f192bcb887fec12510f6cc8e3749
anewcookie <steviecilenti@gmail.com>
2019-10-18 17:26:53 -0400

    fixing left spacing

--- static/ami/js/static.js
+++ static/ami/js/static.js
@@ -1,24 +1,30 @@
 var mySidebar = document.getElementById("mySidebar");
 var overlayBg = document.getElementById("myOverlay");
+var page = document.getElementById("page");
 // Toggle between showing and hiding the sidebar, and add overlay effect
 function w3_open() {
   mySidebar = document.getElementById("mySidebar");
   overlayBg = document.getElementById("myOverlay");
+  page = document.getElementById("page");
   if (mySidebar.style.display === 'block') {
     mySidebar.style.display = 'none';
     overlayBg.style.display = "none";
+    page.style.margin-left = "0px";
   } else {
     mySidebar.style.display = 'block';
     overlayBg.style.display = "block";
+    page.style.margin-left = "300px";
   }
 }
 
 // Close the sidebar with the close button
 function w3_close() {
+  page = document.getElementById("page");
   mySidebar = document.getElementById("mySidebar");
   overlayBg = document.getElementById("myOverlay");
   mySidebar.style.display = "none";
   overlayBg.style.display = "none";
+  page.style.margin-left = "0px";
 }
 
 function statusChange() {
