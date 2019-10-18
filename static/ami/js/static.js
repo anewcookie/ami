b54bb20c9c7ca0d3f434d8fdd2e7982dfda1407c
anewcookie <steviecilenti@gmail.com>
2019-10-18 15:03:25 -0400

    trying key

--- static/ami/js/static.js
+++ static/ami/js/static.js
@@ -26,8 +26,8 @@ function statusChange() {
    	statusText = document.getElementsByClassName('status');
 	gigsText = document.getElementsByClassName('gigs');
    	PMI = document.forms["inspection"]["PMI"].value;
-	gigs = document.querySelectorAll('input[type="checkbox"][class="gig"]:checked').length
-	auto = document.querySelectorAll('input[type="checkbox"][class="auto"]:checked').length
+	gigs = document.querySelectorAll('input.gig:checked').length
+	auto = document.querySelectorAll('input.auto:checked').length
     if (gigs >= 4 || auto > 0) {
         for (var i = 0; i < bars.length; i++)  {
             bars[i].style.backgroundColor="red";
